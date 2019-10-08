35f37826b423d7f4c9527315943310869835e67a
Alfred Aquino <alfredaquino@hotmail.com>
2019-10-08 16:37:09 -0400

    Add files via upload

---
+++ disable_Maps_and_OneDrive.ps1
@@ -0,0 +1,10 @@
+﻿
+
+$mapsbroker_path = "HKLM:\system\currentcontrolset\services\MapsBroker"
+Set-ItemProperty -Path $mapsbroker_path -name "Start" -Value 4
+
+$onesync_keys = Get-ChildItem -path:"HKLM:\system\currentcontrolset\services\onesync*"
+foreach ($key in $onesync_keys) {
+    $path = $key.Name.Replace("HKEY_LOCAL_MACHINE","HKLM:")
+    Set-ItemProperty -Path $path -name "Start" -Value 4
+}
